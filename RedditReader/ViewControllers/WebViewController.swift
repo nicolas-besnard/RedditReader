0c167fd5c38f62ea0cee0593ba1498528459e70d
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-09-02 14:44:32 +0200

    Add WebViewController

---
+++ RedditReader/ViewControllers/WebViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WebViewController.swift
+//  RedditReader
+//
+//  Created by Nicolas Besnard on 02/09/2014.
+//  Copyright (c) 2014 Nicolas Besnard. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController, UIWebViewDelegate
+{
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+}
